bbe69679d8bafb4296673fc75302073a845bfd6b
gazorpazorp85 <paolo.groppi@gmail.com>
2020-11-14 14:58:46 +0200

    Updates

---
+++ precache-manifest.3f9382ca006d69fbfc4902ad9e29e26f.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e3ea36b7c90c1d9390329ed92991bd0e",
+    "url": "/iexhibit/index.html"
+  },
+  {
+    "revision": "54a067085bd051bac06e",
+    "url": "/iexhibit/static/css/main.724b9ebf.chunk.css"
+  },
+  {
+    "revision": "036edb54bef6677a254b",
+    "url": "/iexhibit/static/js/2.8261d0a9.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/iexhibit/static/js/2.8261d0a9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "54a067085bd051bac06e",
+    "url": "/iexhibit/static/js/main.73d4ea27.chunk.js"
+  },
+  {
+    "revision": "580ea7fdec4f1318b1ab",
+    "url": "/iexhibit/static/js/runtime-main.198c0ff5.js"
+  },
+  {
+    "revision": "8ba765ce9580a3bdae76b59130d0ef8a",
+    "url": "/iexhibit/static/media/AmaticSC-Bold.8ba765ce.ttf"
+  },
+  {
+    "revision": "e2b46dd69f54e57767ceef1d5fc8e688",
+    "url": "/iexhibit/static/media/Assistant-Regular.e2b46dd6.ttf"
+  },
+  {
+    "revision": "bbd6c83402ae121cb5f916f5c0c24a50",
+    "url": "/iexhibit/static/media/not_found.bbd6c834.svg"
+  },
+  {
+    "revision": "1eb615dab2a77645ac81b42ef52411f6",
+    "url": "/iexhibit/static/media/under_construction.1eb615da.svg"
+  }
+]);
